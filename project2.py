b704da8e24185dab04768f5dd5e433c08aba51a3
blessing01 <effiongblessing0122@gmail.com>
2025-10-07 22:22:33 +0100

    first commit

---
+++ project2.py
@@ -0,0 +1,16 @@
+import random
+number = random.randint(100,500)
+print("guess a number from 100-500")
+
+while True:
+    
+    guess = int(input("enter a number from 100 - 500: "))
+    if guess< 100 or guess > 500:
+        print("output failed")
+    if guess > number:
+        print("output high,guess lower")
+    elif guess < number:
+        print("output low,guess higher")
+    else:
+        print("output okay")
+           
